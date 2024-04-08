dd10a29b0416822fcf3978c71b78f677b62565cf
cleo <cleo.recchai@gmail.com>
2024-04-08 15:45:39 +0200

    hash

--- src/DataFixtures/Birthday.php
+++ src/DataFixtures/Birthday.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Factory\BirthdayFactory;
+use App\Factory\UserFactory;
 
 class Birthday extends Fixture
 {
@@ -15,5 +16,7 @@ class Birthday extends Fixture
 
         BirthdayFactory::createMany(5);
         //$manager->flush();
+        UserFactory::createMany(10, ['password' => '2y$13$YRgc6wH1CBuIqUbgYA9OveLGDO8CtU6WkKyQErR3J/a4TBi.H8pmW']); //générer 10mp avec le mp en hasher
     }
+    
 }
